3bbabd277bbf0128d6f4e72fa38c541c7e16ee28
huanhou <bolatmmoldir@gmail.com>
2024-03-13 23:10:09 +0500

    modified

---
+++ css/flexbox/tasks/just.js
@@ -0,0 +1,4 @@
+const list = [1, 2, 3, 4, 5, 6];
+
+console.log(list.length);
+``;
--- css/flexbox/tasks/script.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-const preloader = document.querySelector('[data-preaload]');
-
-window.addEventListener('load', function(){
-    preloader.classList.add('loaded');
-    document.body.classList.add('loaded');
-})
